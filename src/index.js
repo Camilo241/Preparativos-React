ed8e55467b3268ffa42d3392f8030a08bbc13a08
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2025-02-11 08:34:14 -0500

    Dia 6 terminado y documentado :construction:

--- src/index.js
+++ src/index.js
@@ -1,152 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Fuction to show month date year
+// creacion de array con las variables que los identifican
+const countries = [
+  { name: 'Finland', city: 'Helsinki' },
+  { name: 'Sweden', city: 'Stockholm' },
+  { name: 'Denmark', city: 'Copenhagen' },
+  { name: 'Norway', city: 'Oslo' },
+  { name: 'Iceland', city: 'Reykjavík' },
+]
 
-const showDate = (time) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  const month = months[time.getMonth()].slice(0, 3)
-  const year = time.getFullYear()
-  const date = time.getDate()
-  return ` ${month} ${date}, ${year}`
-}
-
-// Header Component
-const Header = ({
-  data: {
-    welcome,
-    title,
-    subtitle,
-    author: { firstName, lastName },
-    date,
-  },
-}) => {
+// creamos la variable coountry y le decimos que dentro de ella esta el name y city
+// en el div definimos un h1 que tendra el nombre y un small que tendra la cuidad
+const Country = ({ country: { name, city } }) => {
   return (
-    <header>
-      <div className='header-wrapper'>
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <small>{showDate(date)}</small>
-      </div>
-    </header>
+    <div>
+      <h1>{name}</h1>
+      <small>{city}</small>
+    </div>
   )
 }
 
-// TechList Component
-const TechList = ({ techs }) => {
-  const techList = techs.map((tech) => <li key={tech}>{tech}</li>)
-  return techList
+// creamos una variable que se llama countries y contiene a countries 
+// .map´me permite recorrer el array country y organizarlo
+// key me permite definir la llave que en este caso es name y el country me permite pasar cada pais de country
+// Se devuelve un <div> que contiene la lista de componentes Country.
+const Countries = ({ countries }) => {
+  const countryList = countries.map((country) => (
+    <Country key={country.name} country={country} />
+  ))
+  return <div>{countryList}</div>
 }
 
-// User Card Component
-const UserCard = ({ user: { firstName, lastName } }) => (
-  <div className='user-card'>
-    <h2>
-      {firstName}
-      {lastName}
-    </h2>
+// me crea una variable llamada app
+// contiene 2 div en donde en el primero me define el classname
+// un h1 que me define el titulo de mi matriz 
+// en el countries me va a mostar los paises que tenga en el conuntries 
+
+const App = () => (
+  <div className='container'>
+    <div>
+      <h1>Countries List</h1>
+      <Countries countries={countries} />
+    </div>
   </div>
 )
 
-// A button component
-
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-)
-
-// CSS styles in JavaScript Object
-const buttonStyles = {
-  backgroundColor: '#61dbfb',
-  padding: 10,
-  border: 'none',
-  borderRadius: 5,
-  margin: 3,
-  cursor: 'pointer',
-  fontSize: 18,
-  color: 'white',
-}
-
-// Main Component
-const Main = ({ user, techs, greetPeople, handleTime }) => (
-  <main>
-    <div className='main-wrapper'>
-      <p>Prerequisite to get started react.js:</p>
-      <ul>
-        <TechList techs={techs} />
-      </ul>
-      <UserCard user={user} />
-      <Button text='Greet People' onClick={greetPeople} style={buttonStyles} />
-      <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
-    </div>
-  </main>
-)
-
-// Footer Component
-const Footer = ({ copyRight }) => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright {copyRight.getFullYear()}</p>
-    </div>
-  </footer>
-)
-
-// The App, or the parent or the container component
-// Functional Component
-const App = () => {
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Ahi vamos Aprendiendo',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Camilo',
-      lastName: 'Fandiño',
-    },
-    date: new Date(), // date needs to be formatted to a human readable format
-  }
-  const date = new Date()
-  const techs = ['HTML', 'CSS', 'JavaScript']
-  // copying the author from data object to user variable using spread operator
-  const user = { ...data.author }
-
-  const handleTime = () => {
-    alert(showDate(new Date()))
-  }
-  const greetPeople = () => {
-    alert('Welcome to 30 Days Of React Challenge ' + ' ' + showDate(new Date))
-  }
-
-  return (
-    <div className='app'>
-      <Header data={data} />
-      <Main
-        user={user}
-        techs={techs}
-        handleTime={handleTime}
-        greetPeople={greetPeople}
-      />
-      <Footer copyRight={date} />
-    </div>
-  )
-}
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
