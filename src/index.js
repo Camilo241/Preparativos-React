8d7cfe5f97f0d94185020e0664732d12738520cc
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2025-02-17 21:56:24 -0500

    aprendiendo las props y los metodos dia 7 completado :construction:

--- src/index.js
+++ src/index.js
@@ -1,50 +1,194 @@
+// index.js
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+import asabenehImage from './images/asabeneh.jpg'
 
-// creacion de array con las variables que los identifican
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
+// Fuction to show month date year
 
-// creamos la variable coountry y le decimos que dentro de ella esta el name y city
-// en el div definimos un h1 que tendra el nombre y un small que tendra la cuidad
-const Country = ({ country: { name, city } }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
-    </div>
-  )
-}
-
-// creamos una variable que se llama countries y contiene a countries 
-// .map´me permite recorrer el array country y organizarlo
-// key me permite definir la llave que en este caso es name y el country me permite pasar cada pais de country
-// Se devuelve un <div> que contiene la lista de componentes Country.
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ))
-  return <div>{countryList}</div>
-}
-
-// me crea una variable llamada app
-// contiene 2 div en donde en el primero me define el classname
-// un h1 que me define el titulo de mi matriz 
-// en el countries me va a mostar los paises que tenga en el conuntries 
-
-const App = () => (
-  <div className='container'>
-    <div>
-      <h1>Countries List</h1>
-      <Countries countries={countries} />
-    </div>
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
   </div>
 )
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+// class based component
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    console.log(this.props.data)
+    // crea un array constante en donde agrega las cosas y lo define como this.props.data para guardar los datos
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+// TechList Component
+// class base component
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+// Main Component
+// Class Component
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList techs={this.props.techs} />
+          </ul>
+          <UserCard user={this.props.user} />
+          <Button
+            text='Greet People'
+            onClick={this.props.greetPeople}
+            style={buttonStyles}
+          />
+          <Button
+            text='Show Time'
+            onClick={this.props.handleTime}
+            style={buttonStyles}
+          />
+        </div>
+      </main>
+    )
+  }
+}
+
+// Footer Component
+// Class component
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    // copying the author from data object to user variable using spread operator
+    const user = { ...data.author, image: asabenehImage }
+
+    return (
+      <div className='app'>
+        <Header data={data} />
+        <Main
+          user={user}
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+        />
+
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
+}
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
